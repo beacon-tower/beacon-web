52bf5b60aec925b59fe9883f516d67a13f9fab13
leeicoding <lihk11844@126.coom>
2018-01-17 20:14:35 +0800

    我的钱包

--- pages/wallet.vue
+++ pages/wallet.vue
@@ -7,14 +7,15 @@
         position: relative;
         .icon{
             display:inline-block;
-            width: 16px;
-            height: 16px;
+            width: 20px;
+            height: 20px;
             margin-right: 10px;
-            background: yellow;
+            background: url(../assets/images/fire_coin.png) center;
             vertical-align: middle;
         }
         span{
             font-size: 14px;
+            margin-top: 4px;
             color: #666;
             text-indent: 40px;
             font{
@@ -26,11 +27,67 @@
             position: absolute;
             right: 20px;
         }
+        .button{
+            display: inline-block;
+            width: 60px;
+            line-height: 28px;
+            text-align:center;
+            font-size: 14px;
+            color: #3BEDF4;
+            border: 1px solid #3BEDF4;
+            border-radius: 4px;
+            cursor: pointer;
+            margin-left: 15px;
+        }
     }
     .address{
         margin-top: 60px;
         color: #666666;
         font-size: 14px;
+    }
+    .tabs-component-tabs{
+        border-bottom: 1px solid #E0E0E0;
+    }
+    li.tabs-component-tab{
+        display: inline-block;
+        margin-top: 30px;
+        width: 162px;
+        text-align: center;
+        line-height: 40px;
+        i{
+            color: #666;
+            padding-right: 4px;
+        }
+       span{
+           color: #666;
+           text-indent: 50px;
+       }
+       &.is-active{
+           border-bottom: 2px solid  #003C7A;
+          span{
+           color: #003C7A;
+          }
+          i{
+              color: #003C7A;
+          }
+       }
+    }
+    .tab-content{
+        margin-top: 40px;
+        .select-option{
+            color: #666;
+            #searchArticle{
+                margin-left: 5px;
+                height: 30px;
+            }
+        }
+    }
+    .popup{
+        position:absolute;
+        width: 100%;
+        height: 100%;
+        left: -100%;
+        height: -5
     }
 }
    
@@ -41,17 +98,41 @@
             <div class="icon"></div>
             <span>我的烽火币 <font>500</font>枚</span>
             <span class="historyEarn">历史总计赚取100枚</span>
+            <i class="button">转出</i>
         </div>
         <div class="address">我的钱包：abcdefghijkfgdsifgifgdisufdisgfdilmn</div>
+        <div>
+    
+        <tabs :options ="{useUrlFragment:false}">
+            <tab prefix='<i class="iconfont">&#xe6e7;</i>' name='<span>赚取记录</span>' >
+                <div class="tab-content">赚取记录</div>
+            </tab>
+            <tab prefix='<i class="iconfont">&#xe6e7;</i>' name='<span>转账记录</span>'>
+                <div class="tab-content">
+                    <div class="select-option">
+                        <span>文章:</span><input type="text" id="searchArticle" class="textInput"/>
+                    </div>
+                </div>
+            </tab>
+        </tabs>
+
+        <div class="popup">弹出层</div>
+</div>
     </div>
 </template>
 <script>
+  import {Tabs, Tab} from 'vue-tabs-component';
   export default{
     layout: 'default',
     data(){
         return {
-            items: 123
+            items: 123,
+            tab1: 1,
+            tabs: 2
         }
+    },
+    components: {
+        Tabs, Tab
     },
     methods:{
       changeInterest(type){
