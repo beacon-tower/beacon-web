75dbbee48056466ce3a54383708f3f30e505eae1
xieping <xieping@lixin360.com>
2018-01-18 10:08:42 +0800

    新增Meta兼容性标签和定制html模板

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,8 +3,14 @@ module.exports = {
   ** Headers of the page
   */
   head: {
-    title: 'beacon_web',
+    title: '烽火台',
     meta: [
+      { name: 'keywords', content: '烽火台,dapp,asch'},
+      { name: 'description', content: '国内第一家dapp应用'},
+      { name: 'renderer', content: 'webkit'},
+      { name: 'author', content: '烽火台'},
+      { 'http-equiv': 'X-UA-Compatible', content: 'IE=Edge,chrome=1' },
+      { 'http-equiv': 'Content-Type', content: 'text/html; charset=UTF-8' },
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: 'beacon_web，Nuxt.js project' }
