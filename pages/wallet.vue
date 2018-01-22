ff34dc4fdf67200e69bcd9b96100a990f24e56a6
icodinglee <Lihk11844@126.com>
2018-01-22 22:41:25 +0800

    转账记录

--- pages/wallet.vue
+++ pages/wallet.vue
@@ -81,6 +81,50 @@
                 height: 30px;
             }
         }
+        .search{
+          display: inline-block;
+          width: 64px;
+          line-height: 32px;
+          text-align:center;
+          font-size: 14px;
+          color: #3BEDF4;
+          border: 1px solid #3BEDF4;
+          border-radius: 4px;
+          cursor: pointer;
+          margin-left: 15px;
+        }
+        .tableList{
+          margin-top: 40px;
+          text-align: left;
+          color: #666;
+          table{
+            width: 100%;
+            .header{
+              width: 100%;
+              line-height: 30px;
+              color: #0B4480;
+              border-bottom: 1px solid #E0E0E0
+            }
+            td{
+              line-height: 30px;
+            }
+            .type{
+              width: 15%;
+            }
+            .target{
+              width: 25%;
+            }
+            .time{
+              width: 15%;
+            }
+            .money{
+              width: 15%;
+            }
+            .desc{
+              width: 25%;
+            }
+          }
+        }
     }
     .popup{
         position:fixed;
@@ -159,18 +203,45 @@
         <div>
 
         <tabs :options ="{useUrlFragment:false}">
+
+            <!-- 转账记录 -->
             <tab prefix='<i class="iconfont">&#xe6e7;</i>' name='<span>赚取记录</span>' >
                 <div class="tab-content">赚取记录</div>
             </tab>
+
+            <!-- 转账记录 -->
             <tab prefix='<i class="iconfont">&#xe6e7;</i>' name='<span>转账记录</span>'>
                 <div class="tab-content">
                     <div class="select-option">
-                        <span>文章:</span><input type="text" id="searchArticle" class="textInput"/>
+                        <span>文章:</span><input type="text" id="searchArticle" class="textInput" placeholder="根据文章标题搜索"/>
+                        <div class="search">搜索</div>
                     </div>
+                    <div class="tableList">
+                      <table >
+                        <tr class="header">
+                          <th class="type">交易类型</th>
+                          <th class="target">交易方地址</th>
+                          <th class="time">交易时间</th>
+                          <th class="money">交易金额</th>
+                          <th class="desc">交易说明</th>
+                        </tr>
+                        <tbody>
+                          <tr>
+                            <td class="type">转账</td>
+                            <td class="target">qweqwewqewqewqeqqweq</td>
+                            <td class="time">2017-12-12</td>
+                            <td class="money">20171312</td>
+                            <td lass="desc">转给苏老板</td>
+                          </tr>
+                        </tbody>
+                      </table>
+                    </div>
+
                 </div>
             </tab>
         </tabs>
 
+        <!-- 弹出框，转账 -->
         <div class="popup" v-if="tradeShow">
           <div class="content">
             <h5>资产转出</h5>
@@ -204,6 +275,7 @@
 </template>
 <script>
   import {Tabs, Tab} from 'vue-tabs-component';
+  import DatePicker from '../components/DatePicker';
   export default{
     layout: 'default',
     data(){
@@ -215,7 +287,7 @@
         }
     },
     components: {
-        Tabs, Tab
+        Tabs, Tab, DatePicker
     },
     methods:{
       changeInterest(type){
@@ -227,6 +299,9 @@
       },
       toggleTradeCard(){
         this.tradeShow = !this.tradeShow;
+      },
+      myCallback(e){
+        console.log(e);
       }
     }
   }
